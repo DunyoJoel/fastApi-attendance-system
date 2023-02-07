27fcc0d76a8ddaa491a87e7ca4a8355a7321ce1b
DunyoJoel <brahmixture99@gmail.com>
2023-02-07 14:29:50 +0000

    first commit

---
+++ app/utils/schemas.py
@@ -0,0 +1,114 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+
+
+
+class CreateUser(BaseModel):
+    fullname: str
+    department: str
+    location: str
+    contact: str
+    device: str
+    dateAdded : datetime
+    isActive: bool = None
+
+    class Config():
+        orm_mode = True
+
+class ShowUser(BaseModel):
+    id: int
+    fullname: str
+    department: str
+    location: str
+    contact: str
+    isActive: bool
+    dateAdded: datetime
+    
+    class Config():
+        orm_mode = True
+
+class CreateAttendance(BaseModel):
+    userId: int
+    action: str
+    
+    class Config():
+        orm_mode = True
+
+class ShowAttendance(BaseModel):
+    id: int
+    userId: int
+    action: str
+    actionTime: datetime
+    
+    class Config():
+        orm_mode = True
+
+
+
+class CreateDepartment(BaseModel):
+    department_name: str
+    dateAdded : datetime
+    isActive: bool = None
+
+    class Config():
+        orm_mode = True
+
+class ShowDepartment(BaseModel):
+    id: int
+    department_name: str
+    isActive: bool
+    dateAdded: datetime
+    
+    class Config():
+        orm_mode = True
+
+
+
+
+class CreateAdmin(BaseModel):
+    admin_name: str
+    contact : str
+    email : str
+    password : str
+    dateAdded : datetime
+    isActive: bool = None
+
+    class Config():
+        orm_mode = True
+
+class ShowAdmin(BaseModel):
+    id: int
+    admin_name: str
+    contact : str
+    email : str
+    password : str
+    dateAdded : datetime
+    isActive: bool = None
+    
+    class Config():
+        orm_mode = True
+
+
+
+
+class CreateRole(BaseModel):
+    rolename : str
+    department : str
+    dateAdded : datetime
+    isActive: bool = None
+
+    class Config():
+        orm_mode = True
+
+class ShowRole(BaseModel):
+    id: int
+    rolename : str
+    department: str
+    isActive: bool
+    dateAdded: datetime
+    
+    class Config():
+        orm_mode = True
+
